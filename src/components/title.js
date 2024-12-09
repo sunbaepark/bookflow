00a31aa3621d489b31f318bb3dc44b492dfb5a0d
Jihyun <dns12134@gmail.com>
2024-12-09 11:58:14 +0900

    feat: 프로젝트 제목 컴포넌트 구현

---
+++ src/components/title.js
@@ -0,0 +1,12 @@
+function Title() {
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+      <h1 className="text-6xl font-bold mb-8">Bookflow</h1>
+      <p className="text-xl text-gray-600 text-center">
+        도서관 사서를 위한 최적의 도서 이동 경로를 제공하는 서비스
+      </p>
+    </div>
+  )
+}
+
+export default Title
